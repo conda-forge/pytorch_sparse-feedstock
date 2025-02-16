191d55c838991f47879bc6a4a945cb815f6622e3
h-vetinari <h.vetinari@gmx.com>
2025-02-16 12:11:11 +1100

    add arches for CUDA 12.6 in build.sh; remove obsolete ones

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,23 +3,8 @@
 set -euxo pipefail
 
 if [[ ${cuda_compiler_version} != "None" && "$target_platform" == linux-64 ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0"
     export FORCE_CUDA="1"
-    if [[ ${cuda_compiler_version} == 9.0* ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;7.0+PTX"
-    elif [[ ${cuda_compiler_version} == 9.2* ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0+PTX"
-    elif [[ ${cuda_compiler_version} == 10.* ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5+PTX"
-    elif [[ ${cuda_compiler_version} == 11.0* ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0+PTX"
-    elif [[ ${cuda_compiler_version} == 11.1 ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
-    elif [[ ${cuda_compiler_version} == 11.2 ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
-    elif [[ ${cuda_compiler_version} == 11.8 ]]; then
-        export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
-    elif [[ ${cuda_compiler_version} == 12.0 ]]; then
+    if [[ ${cuda_compiler_version} == 12.6 ]]; then
         export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
     else
         echo "unsupported cuda version. edit build_pytorch.sh"
